bc9b3abadf864cf8949e28f83e1565aa314321b7
calzes <calipz@uw.edu>
2018-12-03 19:28:00 -0800

    sounds in each state

--- main/Active1.cpp
+++ main/Active1.cpp
@@ -15,7 +15,8 @@ uint8_t Active1::getNumRepeats() {
 void Active1::loop(uint32_t dt) {
   //Random number: if it's odd, play smoothRainbow, if even, play quadRand
   rand() % 2 ? Neopixel::setLight(0x02) : Neopixel::setLight(0x10);
-  Midi::setSound(15);
+  //Midi::setSound(15);
+  Midi::setSound(rand() % 10 + 1); // Random bird sound from 1-11
 }
 
 const uint8_t* Active1::getLocalWeights() {
--- main/Active2.cpp
+++ main/Active2.cpp
@@ -13,7 +13,7 @@ uint8_t Active2::getNumRepeats() {
 //Bell lights: &bell(0x04), bell2(0x13)
 void Active2::loop(uint32_t dt) {
   rand() % 2 ? Neopixel::setLight(0x04) : Neopixel::setLight(0x13);
-  Midi::setSound(2);
+  Midi::setSound(rand() % 4 + 0x21); // Random bell sound between 0x20 and 0x24
 }
 
 const uint8_t* Active2::getLocalWeights() {
